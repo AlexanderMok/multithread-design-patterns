f3b2dca72d926d9b7b76c1d386e55a5f3520b6e5
moqinglai <qinglai.mo@shatacloud.com>
2019-08-02 20:11:25 +0800

    change lock in FutureTask

--- src/main/java/com/threading/pattern/future/FutureTask.java
+++ src/main/java/com/threading/pattern/future/FutureTask.java
@@ -1,8 +1,5 @@
 package com.threading.pattern.future;
 
-import java.util.concurrent.locks.LockSupport;
-import java.util.concurrent.locks.ReentrantLock;
-
 /**
  * JDK {@link java.util.concurrent.FutureTask} use AQS.
  *
@@ -15,7 +12,7 @@ public class FutureTask<T> implements Future<T> {
     private T outcome;
     private boolean isDone = false;
 
-    private final ReentrantLock lock = new ReentrantLock();
+    private final Object lock = new Object();
 
     @Override
     public boolean isDone() {
@@ -24,33 +21,28 @@ public class FutureTask<T> implements Future<T> {
 
     @Override
     public T get() throws InterruptedException {
-        lock.lock();
-        try {
+        synchronized (lock) {
             //wait until task is done.
             while (!isDone) {
                 lock.wait();
             }
-        } finally {
-            lock.unlock();
         }
         return outcome;
     }
 
     /**
      * Set/Update {@code result} and {@code isDone} status.
+     *
      * @param result
      */
     protected void finish(T result) {
-        lock.lock();
-        try {
+        synchronized (lock) {
             if (isDone) {
                 return;
             }
             this.outcome = result;
             this.isDone = true;
             lock.notifyAll();
-        } finally {
-            lock.unlock();
         }
     }
 }
