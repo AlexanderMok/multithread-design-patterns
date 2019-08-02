54c28b8151155f97db52d8cd02aab06dbd3e272c
moqinglai <qinglai.mo@shatacloud.com>
2019-08-02 19:42:19 +0800

    implement FutureServiceImpl

--- src/main/java/com/threading/pattern/future/FutureServiceImpl.java
+++ src/main/java/com/threading/pattern/future/FutureServiceImpl.java
@@ -1,19 +1,38 @@
 package com.threading.pattern.future;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Create threads for tasks.
+ *
  * @author Alex
  */
 public class FutureServiceImpl<I, O> implements FutureService<I, O> {
 
     private final static String FUTURE_THREAD_PREFIX = "FUTURE-";
+    private final AtomicInteger next = new AtomicInteger(0);
+
+    private String nextThreadName() {
+        return FUTURE_THREAD_PREFIX + next.getAndIncrement();
+    }
+
     @Override
     public Future<?> submit(Runnable runnable) {
-        return null;
+        final FutureTask<Void> future = new FutureTask<>();
+        new Thread(() -> {
+            runnable.run();
+            future.finish(null);
+        }, nextThreadName()).start();
+        return future;
     }
 
     @Override
     public Future<O> submit(Task<I, O> task, I input) {
-        return null;
+        final FutureTask<O> future = new FutureTask<>();
+        new Thread(() -> {
+            O outcome = task.get(input);
+            future.finish(outcome);
+        }, nextThreadName()).start();
+        return future;
     }
 }
